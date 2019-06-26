6dc1b07f4231b8074f5476581df14d9dbb1b8dd5
MariosTheof <athmarios@gmail.com>
2019-06-26 16:01:37 +0100

    Changed to filtered curl with greps

--- curlBash.sh
+++ curlBash.sh
@@ -5,23 +5,8 @@ function read_dom {
 	read -d \< ENTITY CONTENT
 }
 
-#while read_dom; do
-#	if [[ $ENTITY = "title" ]]; then
-#		echo $CONTENT
-#		exit
-#	fi
-#done < result.html > result.txt
+url=https://practicalguidetoevil.wordpress.com/table-of-contents/
 
-while read_dom; do
-	echo "$ENTITY => $CONTENT"
-done < result.html
-
-#url=https://practicalguidetoevil.wordpress.com/table-of-contents/
-
-#response=$(curl ${url})
-
-#curl $url > result.html
-
-#echo "$response" | grep Book
+curl $url | grep -Eoi '<a [^>]+>' | grep -Eo 'href="[^\"]+"' | grep -Eo '(http|https)://[^"]+' | grep chapter > result.html
 
 
