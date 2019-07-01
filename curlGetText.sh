e8d9bb17f00cdfba28cebf1cb4cd0c6eb65d87e7
MariosTheof <athmarios@gmail.com>
2019-07-01 12:42:50 +0100

    Filters txt to proper utf-8 encoding

--- curlGetText.sh
+++ curlGetText.sh
@@ -7,11 +7,11 @@ function read_dom {
 
 url=https://practicalguidetoevil.wordpress.com/2015/04/08/chapter-2-invitation/
 
-curl $url > resultFromChapter2.html
+curl $url | recode -i html..utf8 > resultFromChapter2.html
 
 while read_dom; do
-	if [[ $ENTITY = 'span style="font-size:medium;"' ]]; then
-		echo "$ENTITY => $CONTENT"  
+	if [[ $ENTITY = 'span style="font-size:medium;"' ]]; then		
+		echo ${CONTENT//â/\"}
 	fi
 done < resultFromChapter2.html > tsekare.txt
 
